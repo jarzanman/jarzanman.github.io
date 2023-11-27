d0fcc5ede4a7f2176fb59048a3ce047a70baf067
jarzanman <jarzanman@gmail.com>
2023-11-26 19:16:55 -0500

    js fun

--- assets/js/dropcap.js
+++ assets/js/dropcap.js
@@ -1,27 +1,5 @@
-const fonts = ["GoudyIni", "TypographerWoodcut", "CheshireInitials", "Exotica"];
-let random = fonts[Math.floor(Math.random() * fonts.length)];
-//random = fonts[2]
-let dc = document.querySelectorAll(".dc");
-console.log(dc);
-switch (random) {
-  case "GoudyIni":
-    dc.forEach((e) => {
-      e.classList.add("goudyini");
-    });
-    break;
-  case "TypographerWoodcut":
-    dc.forEach((e) => {
-      e.classList.add("typographerwoodcut");
-    });
-    break;
-  case "CheshireInitials":
-    dc.forEach((e) => {
-      e.classList.add("cheshireinitials");
-    });
-    break;
-  case "Exotica":
-    dc.forEach((e) => {
-      e.classList.add("exotica");
-    });
-    break;
-}
+const fonts = ['goudy', 'woodcut', 'cheshire', 'exotica', 'floral'];
+let random=fonts[Math.floor(Math.random()*fonts.length)];let dc=document.querySelectorAll('.dc');
+//random=fonts[4];
+dc.forEach((e)=>{e.classList.add(random)})
+
